8d12c855d0cff91cb16739564a84d7cfe443d1ea
Dibyendu Mitra Roy <dibs439@gmail.com>
2014-09-24 15:42:30 +0530

    first commit

---
+++ js/val.js
@@ -0,0 +1,36 @@
+
+var pumpArray = [];
+var fillerArray = [];
+var carTypeArray = [];
+var carOwnerArray = [];
+var additiveArray = [];
+
+pumpArray[1] = "Pump 1";
+pumpArray[2] = "Pump 2";
+pumpArray[3] = "Pump 3";
+
+fillerArray[1] = "000123";
+fillerArray[2] = "000124";
+fillerArray[3] = "000125";
+fillerArray[4] = "000126";
+fillerArray[5] = "000127";
+fillerArray[6] = "000128";
+fillerArray[7] = "000129";
+
+carTypeArray[1] = "Ford";
+carTypeArray[2] = "Chevy";
+carTypeArray[3] = "Jeep";
+carTypeArray[4] = "Subraru";
+carTypeArray[5] = "Benz";
+carTypeArray[6] = "UNK - Unknown";
+
+carOwnerArray[1] = "Jon Jones";
+carOwnerArray[2] = "Dave Jones";
+carOwnerArray[3] = "Brenda Jones";
+carOwnerArray[4] = "Billy Gram";
+carOwnerArray[5] = "Tony Bennette";
+carOwnerArray[6] = "Jimmy Good";
+carOwnerArray[7] = "Upto Nogood";
+
+additiveArray[1] = "Ethanol";
+additiveArray[2] = "Type A Lubricant";
